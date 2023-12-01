385dfb4d386481031db694f8cfa9ade85b712fa4
Kien Tuong Truong <info@ktruong.dev>
2023-12-01 17:24:33 +0100

    feat: day01 PAIN

---
+++ day1/main.py
@@ -0,0 +1,52 @@
+from string import digits
+
+def part1(inp: list[str]):
+    subs_table = {}
+    for d in digits:
+        subs_table[d] = int(d)
+
+    s = 0
+    for line in inp:
+        n = []
+        for i in range(len(line)):
+            for k, v in subs_table.items():
+                if line[i:].startswith(k):
+                    n.append(v)
+        s += n[0] * 10 + n[-1]
+        n = []
+    return s
+
+def part2_alt(inp: list[str]):
+    subs_table = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
+    for d in digits:
+        subs_table[d] = int(d)
+
+    s = 0
+    for line in inp:
+        n = []
+        for i in range(len(line)):
+            for k, v in subs_table.items():
+                if line[i:].startswith(k):
+                    n.append(v)
+        s += n[0] * 10 + n[-1]
+        n = []
+    return s
+
+
+if __name__ == "__main__":
+    with open("1.in") as f:
+        inp = f.readlines()
+
+    print(f"Part 1: {part1(inp)}")
+    print(f"Part 2: {part2_alt(inp)}")
